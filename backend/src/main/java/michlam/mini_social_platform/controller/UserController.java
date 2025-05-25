ee7bf2486124373ba42c89877a6e6783982293ab
UBC Student <lam.michael.81201@gmail.com>
2025-05-25 15:37:34 -0700

    Started stubs for pfp apis

--- backend/src/main/java/michlam/mini_social_platform/controller/UserController.java
+++ backend/src/main/java/michlam/mini_social_platform/controller/UserController.java
@@ -6,6 +6,7 @@ import michlam.mini_social_platform.dto.UserDto;
 import michlam.mini_social_platform.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @AllArgsConstructor
 @RestController
@@ -38,5 +39,23 @@ public class UserController {
     public ResponseEntity<Object> updateUser(@PathVariable Long userId, @RequestBody UserDto userDto) {
         return null;
     }
+
+
+    // TODO: GetProfilePicture REST API
+    @GetMapping("{id}/pfp")
+    public ResponseEntity<Object> getProfilePicture(@PathVariable Long userId) {
+        return null;
+    }
+
+    // TODO: UpdateProfilePicture REST API
+    @PutMapping("{id}/pfp")
+    public ResponseEntity<Object> updateProfilePicture(
+            @PathVariable Long userId,
+            @RequestParam("pfp") MultipartFile pfp
+    ) {
+        return null;
+    }
+
+
 
 }
