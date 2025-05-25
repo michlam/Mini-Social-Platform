b677e1f4707cfaea5dca64f8e6ff312568fad803
UBC Student <lam.michael.81201@gmail.com>
2025-05-25 16:00:02 -0700

    Added CreateUser Rest API

--- backend/src/main/java/michlam/mini_social_platform/controller/UserController.java
+++ backend/src/main/java/michlam/mini_social_platform/controller/UserController.java
@@ -3,7 +3,14 @@ package michlam.mini_social_platform.controller;
 import lombok.AllArgsConstructor;
 import michlam.mini_social_platform.config.JwtService;
 import michlam.mini_social_platform.dto.UserDto;
+import michlam.mini_social_platform.entity.User;
+import michlam.mini_social_platform.exception.DuplicateResourceException;
+import michlam.mini_social_platform.exception.ErrorResponse;
+import michlam.mini_social_platform.mapper.Mapper;
 import michlam.mini_social_platform.service.UserService;
+import org.apache.coyote.Response;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,7 +26,22 @@ public class UserController {
     // TODO: CreateUser REST API
     @PostMapping
     public ResponseEntity<Object> createUser(@RequestBody UserDto userDto) {
-        return null;
+        try {
+            UserDto savedUserDto = userService.createUser(userDto);
+            User savedUser = Mapper.mapToUser(savedUserDto);
+            Long userId  = userService.getIdByUsername(savedUser.getUsername());
+
+            // Generate the JSON Web Token
+            String jwtToken = jwtService.generateToken(savedUser);
+            return new ResponseEntity<>(
+                    AuthenticationResponse.builder().userId(userId).token(jwtToken).build(),
+                    HttpStatus.CREATED
+            );
+
+        } catch (DuplicateResourceException e) {
+            ErrorResponse response = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
     }
 
     // TODO: GetUsers REST API
