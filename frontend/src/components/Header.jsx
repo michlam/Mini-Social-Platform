02bcd8ccc085e35d04165c8b153babc1d0f5a527
UBC Student <lam.michael.81201@gmail.com>
2025-05-29 17:31:32 -0700

    Started basic header structuring

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,26 @@
+import { Link, NavLink } from "react-router-dom";
+
 export default function Header() {
+    const activeStyles = {
+        fontWeight: "700",
+    }
+
     return (
         <header>
-            
+            <Link className="site-logo" to="/">LOGO</Link>
+            <nav>
+                <NavLink to="home" style={({isActive}) => isActive ? activeStyles : null} className={"navlink"}>
+                    Home
+                </NavLink>
 
+                <NavLink to="messages" style={({isActive}) => isActive ? activeStyles : null} className={"navlink"}>
+                    Messages
+                </NavLink>
+
+                <NavLink to="profile" style={({isActive}) => isActive ? activeStyles : null} className={"navlink"}>
+                    Profile
+                </NavLink>
+            </nav>
         </header>
     )
 }
